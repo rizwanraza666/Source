c758227a8c5f349aab10163395e6108ac479b124
rizwanraza666 <49280732+rizwanraza666@users.noreply.github.com>
2024-12-04 23:35:41 +0500

    AddFilesinModels

---
+++ PurchaseSystem/PurchaseSystem/Models/Issuance.cs
@@ -0,0 +1,12 @@
+﻿namespace PurchaseSystem.Models
+{
+    public class Issuance
+    {
+        [key]
+        public int id { get; set; }
+        public DateTime iss_date { get; set; }
+        public string? emp_name { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
---
+++ PurchaseSystem/PurchaseSystem/Models/Purchase.cs
@@ -0,0 +1,16 @@
+﻿namespace PurchaseSystem.Models
+{
+    public class Purchase
+    {
+        [key]
+        public int id { get; set; }
+        public string? pur_item { get; set; }
+        public int Qty { get; set; }
+        public DateTime pur_date { get; set; }
+        public int price { get; set; }
+        public string? vendor { get; set; }
+
+
+
+    }
+}
