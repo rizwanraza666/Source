b063aac110d72675108924d9899e4782301a6d80
Rizwan Raza <rizwanraza666@gmail.com>
2024-12-07 17:35:51 +0500

    DotnetCore

--- PurchaseSystem/PurchaseSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PurchaseSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
